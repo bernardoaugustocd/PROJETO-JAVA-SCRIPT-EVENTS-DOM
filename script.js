62b87adc19e4ab414f32108626284eaf723dcd55
SITE_JURIDICO <bernardoaugustocd@gmail.com>
2026-02-09 21:37:48 -0300

    PROJETO-JAVA-SCRIPT-EVENTS-DOM|Bernardo:  Criação de novas condicionais no java script e tag para acionar o botão na tecla enter. Mudança de título das páginas html e adição de id para o botão.

--- script.js
+++ script.js
@@ -9,6 +9,29 @@
         mensagem.innerHTML = "Selecione o tipo de usuário";
     }
 
-    
+    if (usuario === "1" && senha === "123") {
+        alert("Você será direcionado para a página de vendas");
+        window.location.href = "vendas.html";
+    }
+
+    else if (usuario === "2" && senha === "456") {
+        alert("Você será direcionado para a página de estoque");
+        window.location.href = "estoque.html";
+    }
+
+    else if (usuario === "3" && senha === "789") {
+        alert("Você será direcionado para a página adiministrativo");
+        window.location.href = "adiministrativo.html";
+    }
+
+    else {
+        mensagem.innerHTML = "Usuário e/ou senha inválidos. Tente novamente.";
+    }
 
 }
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    senha();
+  }
+});
